fb46b5d8cefbe1d92b61ff7687597413270baacc
Serra Sönmez <serrasonmez@Serra-MacBook-Pro.local>
2017-11-29 02:06:31 +0300

    stars added

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,9 +10,14 @@ import UIKit
 @IBDesignable class RatingControl: UIStackView {
     
     //MARK: Properties
+    
     private var ratingButtons = [UIButton]()
     
-    var rating = 0
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
     
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0)
         {
@@ -45,7 +50,23 @@ import UIKit
     //MARK: Button Action
     
     @objc func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        func ratingButtonTapped(button: UIButton) {
+            guard let index = ratingButtons.index(of: button) else {
+                fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+            }
+            
+            // Calculate the rating of the selected button
+            let selectedRating = index + 1
+            
+            if selectedRating == rating {
+                // If the selected star represents the current rating, reset the rating to 0.
+                rating = 0
+            } else {
+                // Otherwise set the rating to the selected star
+                rating = selectedRating
+            }
+        }
+        
     }
     
     //MARK: Private Methods
@@ -59,11 +80,22 @@ import UIKit
         }
         ratingButtons.removeAll()
         
+        // Load Button Images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
         for _ in 0..<starCount {
         // Create the button
         let button = UIButton()
-        button.backgroundColor = UIColor.red
         
+        // Set the button images
+        button.setImage(emptyStar, for: .normal)
+        button.setImage(filledStar, for: .selected)
+        button.setImage(highlightedStar, for: .highlighted)
+        button.setImage(highlightedStar, for: [.highlighted, .selected])
+            
         // Add constraints
         button.translatesAutoresizingMaskIntoConstraints = false
         button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
@@ -78,7 +110,19 @@ import UIKit
         // Add the new button to the rating button array
             ratingButtons.append(button)
     }
-}
+        updateButtonSelectionStates()
+        
+    }
+    
+    private func updateButtonSelectionStates() {
+        
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
+        
+    }
+    
 }
 
 
